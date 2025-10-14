c199bffecbeaa59f5d7b36d82054811cbb84ccdd
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2025-10-14 09:56:53 -0400

    The list of plants is now visible.

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, loading, error }) {
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  if (!plants.length) return <p>No plants available.</p>;
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plant) => (
+        <li key={plant.id}>
+          <PlantCard name={plant.name} image={plant.image} price={plant.price} />
+        </li>
+      ))}
+    </ul>
   );
 }
 
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchPlants = () => {
+    setLoading(true);
+    setError(null);
+
+    fetch("http://localhost:6001/plants")
+      .then((r) => {
+        if (r.ok) {
+          return r.json();
+        } else {
+          throw new Error("Fetch request failed");
+        }
+      })
+      .then((data) => setPlants(data))
+      .catch((err) => {
+        setError("Failed to fetch data. Please try again or contact support");
+        console.error(err);
+      })
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchPlants();
+  }, []);
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} loading={loading} error={error} />
     </main>
   );
 }
